1a0d80075882a2417947231df4e3dfde7ea20f28
liyongfei <liyongfei@lookingsky.com>
2018-06-20 13:02:25 +0800

    main method

---
+++ src/main/java/com/liyongfei/main/Main.java
@@ -0,0 +1,11 @@
+package com.liyongfei.main;
+
+/**
+ * @author liyongfei
+ * @date 2018/6/20
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println("this is main method!");
+    }
+}
