fdbb98ec2cdf2f8f6e7a96cf43c3519cfb7b3407
liyongfei <liyongfei@lookingsky.com>
2018-06-21 09:15:15 +0800

    修改测试用例-08

--- src/test/java/com/liyongfei/main/MainTest.java
+++ src/test/java/com/liyongfei/main/MainTest.java
@@ -18,7 +18,7 @@ public class MainTest {
     @Test
     public void testSay() throws Exception {
         Main main = new Main();
-        Assert.assertEquals(main.say("hello"),"hello7");
+        Assert.assertEquals(main.say("hello"),"hello");
     }
 
 }
